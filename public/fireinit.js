76b8f38d07186907350f9777594cebfc59fcc8a8
ITKOO <j4550118@gmail.com>
2017-10-22 23:52:00 +0900

    #1 First Commit

---
+++ public/fireinit.js
@@ -0,0 +1,12 @@
+// Initialize Firebase
+var config =
+{
+  apiKey: "AIzaSyBtNSeEwh7u0kbJ0LihQplcQzEYDobwYD4",
+  authDomain: "cmp-java.firebaseapp.com",
+  databaseURL: "https://cmp-java.firebaseapp.com",
+  projectId: "cmp-java",
+  storageBucket: "gs://cmp-java.appspot.com",
+  messagingSenderId: "888269883027"
+};
+
+firebase.initializeApp(config);
